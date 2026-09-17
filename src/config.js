200d7f5cee09a749d8e4f4d5e7d1c5ece1f5dd6a
houshasei <Houshasei@yandex.com>
2026-09-18 01:13:18 +0800

    Update user configuration IDs in config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const USER_CONFIG = {
-  Ces: { id: 'AKfycbyV0eO2y9PR5KBrn1GWI-7umZH2wZIZW4FE1S0A39WEfgnoB7GkgBAW3D8eioTW_wsJVw' },
-  KB: { id: 'AKfycbxQV2M9C208IbCyL0zC978fQlDYTc-hr0sMdGsjZTjbU0qTPQVvX42ebrSwCUUBi7wsAQ' },
+  Ces: { id: 'AKfycbwsIWT7n8-UgEHNx6qyiMdc1UIsO14udUlQdiJq4-HAMQoJQNPMSDhVILwiFYNwti9kMw' },
+  KB: { id: 'AKfycbwRTV3xrDr5oLW16TDoXwoqBZA-wVZsQ7aRoscXc5POxLHFGrulD-Z3x0an8rMPfuht' },
   JayAr: { id: 'AKfycbwDsMYk8VTcOR3_DzBCLyMb1DmxpRodqpEvk568aFM4KfBinug1fi7d5FHq4ry_XPxk' },
   LJ: { id: 'AKfycbwL5hpGZ2vq7nos3V7vouEEgnaeKgoVPN9w7MEavlM6YGgSIYhwg6VpFGpv5mkitt_R' },
   EJ: { id: 'AKfycbw66O-DXukXjUij-6cuG1af8Cu_p3yzIkXFOGCm_mjARDoEXfqLlFg_E-uw09iP90sk' },
   Yali: { id: 'AKfycbzlC1a9Cs32q1oMrpNpoxGyMe-_-4I5kXhsfE96dPfLjPAUggPrhDqOl9Esv8TV3PA9'},
-  Rub: { id: 'AKfycbzJ64VAmYdKQmIpYuSFVvJNNR3u653StVxNLetksBLAeak0tS-AQTm85DluNWms1vay'},
-  Bobot: { id: 'AKfycbw_r1Po-LnAO1PkWBE9AD3wJf6sWyBA0pVMG9A403vFunmC3TnZ45K10S6kqqY14KlB'},
+  Rub: { id: 'AKfycbz_-vsE2bbJwwOszWwVS0RdpmE8wdFUszLxp61sZZ1TqfMUObfzhL9oCaiP3Yuub3dtzg'},
+  Bobot: { id: 'AKfycbyLDRCWwWd6oT5SMNcr0UleaGnMRsbdwshHtziaoOChkY3MZCGavFzgnyPc4kBiU0rx'},
 };
 
 export const USERS = Object.keys(USER_CONFIG);
